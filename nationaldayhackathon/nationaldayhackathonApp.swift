78e6b20205806a4bdc9bea5cb9d945b3ce1c0d8e
Gabriel Tan <gabriel.tanhk@gmail.com>
2023-08-26 09:51:33 +0800

    Initial Commit

---
+++ nationaldayhackathon/nationaldayhackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  nationaldayhackathonApp.swift
+//  nationaldayhackathon
+//
+//  Created by Jonaven Tan on 26/8/23.
+//
+
+import SwiftUI
+
+@main
+struct nationaldayhackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
